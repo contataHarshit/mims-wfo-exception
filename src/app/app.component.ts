3f620b6b735848126f74f86fae6a8f1e3339bb1f
Harshit Srivastava <harshits@pallav-lap.contatan.local>
2025-10-09 15:10:24 +0530

    route change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
 import { CommonService } from '../service/common.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
   title = 'Mims-exception-wfo';
-
+  tabs = [
+    { label: 'Create WFO Exception Request', path: '', isActive: false },
+    { label: 'WFO Dashboard', path: 'dashboard', isActive: false }
+  ]
   constructor(
     public commonService: CommonService,
     private router: Router
-  ) {}
+  ) { }
+  ngDoCheck(): void {
+    this.tabs.forEach(tab => {
+      tab.isActive = this.isActive(tab.path)
+    })
+  }
 
   navigateTo(path: string) {
     this.router.navigate([path]);
   }
 
   isActive(path: string): boolean {
-    return this.router.url === '/' + path || (path === '' && this.router.url === '/');
+    const currentUrl = this.router.url.replace(/^\/+/, '');
+    const cleanedPath = path.replace(/^\/+/, '');
+
+    if (cleanedPath === '') {
+      return currentUrl === '' || currentUrl === 'create-wfo-exception-request';
+    }
+
+    return currentUrl === cleanedPath;
   }
 }
